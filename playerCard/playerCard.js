b6a48bfce89afaa7ef5e931ae355763543a6a77c
liambut <liampb@outlook.com>
2022-03-20 15:48:58 +0000

    added image cycling on hover

--- playerCard/playerCard.js
+++ playerCard/playerCard.js
@@ -1,5 +1,8 @@
 var playerCardTimeouts = [];
+cancelCycle = true;
 function playerHover(img, playerDetails){
+	cancelCycle = false;
+	clearTimers;
 	img.style = "border-radius: 0%; transition: border-radius 0.5s;";
 	position = playerDetails.querySelector('#position');
 	DOB = playerDetails.querySelector('#DOB');
@@ -16,12 +19,7 @@ function playerHover(img, playerDetails){
 	stat3Height = playerDetails.querySelector('#stat3Height');
 	stat4Weight = playerDetails.querySelector('#stat4Weight');
 	stat5Teams = playerDetails.querySelector('#stat5Teams');
-	if(playerCardTimeouts.length > 0){
-		for (var i = 0; i < playerCardTimeouts.length; i++) {
-			clearTimeout(playerCardTimeouts[i]);
-		}
-		playerCardTimeouts = [];
-	}
+	cycleImg(img, playerDetails, 1);
 	stat1Position.style = "transform: translateX(100%); transition: transform ease-out 0.5s;";
 	stat2DOB.style = "transform: translateX(100%); transition: transform ease-out 0.5s;";
 	stat3Height.style = "transform: translateX(100%); transition: transform ease-out 0.5s;";
@@ -33,7 +31,7 @@ function playerHover(img, playerDetails){
 	playerCardTimeouts.push(setTimeout(() => {stat4Weight.style = "transform: translateX(-100%);";}, 495));
 	playerCardTimeouts.push(setTimeout(() => {stat5Teams.style = "transform: translateX(-100%);";}, 495));
 	playerCardTimeouts.push(setTimeout(() => {stat1Position.innerHTML = stat1.innerHTML;}, 500));
-	playerCardTimeouts.push(setTimeout(() => {stat1Position.style = "transform: translateX(0); transition: transform ease-out 0.5s;";}, 500));
+	playerCardTimeouts.push(setTimeout(() => {stat1Position.style = "transform: translateX(0); transition: transform ease-out 0.5s; font-size:medium;";}, 500));
 	playerCardTimeouts.push(setTimeout(() => {stat2DOB.innerHTML = stat2.innerHTML;}, 500));
 	playerCardTimeouts.push(setTimeout(() => {stat2DOB.style = "transform: translateX(0); transition: transform ease-out 0.5s;";}, 500));
 	playerCardTimeouts.push(setTimeout(() => {stat3Height.innerHTML = stat3.innerHTML;}, 500));
@@ -44,6 +42,9 @@ function playerHover(img, playerDetails){
 	playerCardTimeouts.push(setTimeout(() => {stat5Teams.style = "transform: translateX(0); transition: transform ease-out 0.5s; font-size:medium;";}, 500));
 }
 function playerHoverOff(img, playerDetails){
+	cancelCycle = true;
+	clearTimers;
+	img.src = playerDetails.querySelector('#img1').src;
 	img.style = "border-radius: 50%; transition: border-radius 0.5s;";
 	position = playerDetails.querySelector('#position');
 	DOB = playerDetails.querySelector('#DOB');
@@ -60,13 +61,8 @@ function playerHoverOff(img, playerDetails){
 	stat3Height = playerDetails.querySelector('#stat3Height');
 	stat4Weight = playerDetails.querySelector('#stat4Weight');
 	stat5Teams = playerDetails.querySelector('#stat5Teams');
-	if(playerCardTimeouts.length > 0){
-		for (var i = 0; i < playerCardTimeouts.length; i++) {
-			clearTimeout(playerCardTimeouts[i]);
-		}
-		playerCardTimeouts = [];
-	}
-	stat1Position.style = "transform: translateX(100%); transition: transform ease-out 0.5s;";
+	img.src = playerDetails.querySelector('#img1').src;
+	stat1Position.style = "transform: translateX(100%); transition: transform ease-out 0.5s; font-size:medium;";
 	stat2DOB.style = "transform: translateX(100%); transition: transform ease-out 0.5s;";
 	stat3Height.style = "transform: translateX(100%); transition: transform ease-out 0.5s;";
 	stat4Weight.style = "transform: translateX(100%); transition: transform ease-out 0.5s;";
@@ -86,4 +82,35 @@ function playerHoverOff(img, playerDetails){
 	playerCardTimeouts.push(setTimeout(() => {stat4Weight.style = "transform: translateX(0); transition: transform ease-out 0.5s;";}, 500));
 	playerCardTimeouts.push(setTimeout(() => {stat5Teams.innerHTML = teams.innerHTML;}, 500));
 	playerCardTimeouts.push(setTimeout(() => {stat5Teams.style = "transform: translateX(0); transition: transform ease-out 0.5s;";}, 500));
+}
+function cycleImg(img, playerDetails, imgCount){
+	if(cancelCycle == false){
+		do{
+			imgCount = imgCount + 1;
+			if(imgCount > 5){
+				imgCount = 1;
+			}
+			imgSRC = "#img" + imgCount.toString(); 
+		}while(isImg(playerDetails.querySelector(imgSRC).src) == false);
+		img.src = playerDetails.querySelector(imgSRC).src;
+		playerCardTimeouts.push(setTimeout(() => {cycleImg(img, playerDetails, imgCount)}, 3000));
+	}
+}
+function isImg(file){
+	const acceptedImageTypes = ['gif', 'jpeg', 'png', 'jpg'];
+	let hasImg = false;
+	for(let i = 0; i < 4; i++){
+		if(file.includes(acceptedImageTypes[i])){
+			hasImg = true;
+		}
+	}
+    return hasImg;
+}
+function clearTimers(){
+	if(playerCardTimeouts.length > 0){
+		for (var i = 0; i < playerCardTimeouts.length; i++) {
+			clearTimeout(playerCardTimeouts[i]);
+		}
+		playerCardTimeouts = [];
+	}
 }
